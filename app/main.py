22e9ca7b433a0fe9f2be485aee63a428a22da0a9
Gabriel Bitti <gabrielbitti0@gmail.com>
2025-09-08 14:23:00 -0300

    Adds basic asgi server in main

---
+++ app/main.py
@@ -0,0 +1,16 @@
+"""Main implementation."""
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# todo: configure CORS
+
+@app.get('/api/v1/hello')
+def read_root():
+    return {'Hello': 'World'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=7000)
