dabb647fec621ce4d19e6131a5b6fc7a9bbf5126
Gabriel Bitti <gabrielbitti0@gmail.com>
2025-09-09 20:58:01 -0300

    Fixes alembic target_metadata to my sqlalchemy Base

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from app.settings import POSTGRES_URL
+from app.database import Base
+from app.models import customer  # Import models to register them
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +26,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 
-target_metadata = None
+target_metadata = Base.metadata
 
 
 # other values from the config, defined by the needs of env.py,
