7dddc7234aed27bbad15c55c55f5159010939ff5
Gabriel Bitti <gabrielbitti0@gmail.com>
2025-09-10 22:01:28 -0300

    Feat: route migration

---
+++ alembic/versions/afef1d17c623_create_table_route.py
@@ -0,0 +1,37 @@
+"""create table route
+
+Revision ID: afef1d17c623
+Revises: 837af92bc942
+Create Date: 2025-09-10 22:00:25.269861
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = 'afef1d17c623'
+down_revision: Union[str, Sequence[str], None] = '837af92bc942'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade."""
+    op.create_table('route',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=True),
+        sa.Column('is_active', sa.Boolean(), nullable=False),
+        sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+        sa.Column('updated_at', sa.DateTime(timezone=True), nullable=True),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_route_id'), 'route', ['id'], unique=False)
+
+
+def downgrade() -> None:
+    """Downgrade."""
+    op.drop_index(op.f('ix_route_id'), table_name='route')
+    op.drop_table('route')
