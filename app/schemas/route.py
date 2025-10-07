493ae608aa2a5fd80bb182d726237e87334c0f01
Gabriel Bitti <gabriel.viana@ish.com.br>
2025-10-06 21:11:00 -0300

    Fix: optional datetime, fix other fields, customer e route

--- app/schemas/route.py
+++ app/schemas/route.py
@@ -14,23 +14,23 @@ class CreateRoute(BaseModel):
     name: str
     is_active: bool = True
 
-    # class Config:
-    #     from_attributes = True
+    class Config:
+        from_attributes = True
 
 
 class Route(CreateRoute):
     """Schema for route response."""
 
-    id: str
-    created_at: datetime
-    updated_at: datetime
+    id: int
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
 
 
 """RoutePoint"""
 
 
 class CreateRoutePoint(BaseModel):
-    """Schema for creating a route."""
+    """Schema for creating a route point."""
 
     route_id: int
     address_id: int
@@ -39,36 +39,36 @@ class CreateRoutePoint(BaseModel):
     distance_to_next: float
     notes: Optional[str] = None
 
-    # class Config:
-    #     from_attributes = True
+    class Config:
+        from_attributes = True
 
 
 class RoutePoint(CreateRoutePoint):
-    """Schema for route response."""
+    """Schema for the response."""
 
     id: int
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
 
 
 """RouteSchedule"""
 
 
 class CreateRouteSchedule(BaseModel):
-    """Schema for creating a route."""
+    """Schema for creating a route schedule."""
 
     route_id: int
     status: str
     schedule_date: datetime
-    finish_date: Optional[str] = None
+    finish_date: Optional[datetime]
 
-    # class Config:
-    #     from_attributes = True
+    class Config:
+        from_attributes = True
 
 
 class RouteSchedule(CreateRouteSchedule):
-    """Schema for route response."""
+    """Schema for the response."""
 
     id: int
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
