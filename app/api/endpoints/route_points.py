76dc9d9410fa3bdabb130d1635b9671687f7a4a6
Gabriel Bitti <gabrielbitti0@gmail.com>
2025-10-20 20:47:50 -0300

    Feat: adds route and address validation in route-point creation

--- app/api/endpoints/route_points.py
+++ app/api/endpoints/route_points.py
@@ -2,11 +2,12 @@
 
 from typing import List
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 from app.commons import get_db
-from app.models.route import RoutePoint
+from app.models.address import AddressDTO
+from app.models.route import RoutePoint, RouteDTO
 from app.schemas.route_point import RoutePointOut, CreateRoutePoint
 
 router = APIRouter(tags=['Route Points'])
@@ -17,6 +18,20 @@ router = APIRouter(tags=['Route Points'])
 def create_route_point(route_point: CreateRoutePoint,
                        db: Session = Depends(get_db)):
     """Create a new route_point."""
+    route = RouteDTO(db).get_by_id(route_point.route_id)
+    if not route:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Route not found"
+        )
+
+    address = AddressDTO(db).get_by_id(route_point.address_id)
+    if not address:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Address not found"
+        )
+
     db_route_point = RoutePoint(**route_point.model_dump())
     db.add(db_route_point)
     db.commit()
