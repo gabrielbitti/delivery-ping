57736fb2a7e67e6a62229702697e770e72c70061
Gabriel Bitti <gabrielbitti0@gmail.com>
2025-10-21 20:54:46 -0300

    Fix: http status code when get invalid cellphone format

--- app/domain/customer.py
+++ app/domain/customer.py
@@ -15,7 +15,7 @@ class CustomerDomain:
         cellphone = self.data.get("cellphone")
         if cellphone and (len(cellphone) > 14 or len(cellphone) < 11):
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
+                status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid cellphone format"
             )
 
